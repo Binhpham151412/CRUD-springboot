23ea89dc7a50a4b5ba8bbccd4b9e4d7ad24c0f61
Binhpham151412 <binhpham.201198@gmail.com>
2022-10-05 21:20:37 +0700

    Update UserController

--- src/main/java/com/crudexample/controllers/UserController.java
+++ src/main/java/com/crudexample/controllers/UserController.java
@@ -3,13 +3,12 @@ package com.crudexample.controllers;
 import com.crudexample.user.User;
 import com.crudexample.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -38,9 +37,11 @@ public class UserController {
         return modelAndView;
     }
 
+
     @PostMapping(value = "/users/save")
-    public String saveUser(User user){
+    public String saveUser(User user, RedirectAttributes ra){
         userService.save(user);
+        ra.addFlashAttribute("message","the user has been saved successfully");
         return "redirect:/users";
     }
 
